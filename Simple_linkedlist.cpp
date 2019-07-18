7012c5fa7ebf86d0fe61ae945fa4774cfc46c3cd
Narasimha Reddy MV <39734170+Narasimha012@users.noreply.github.com>
2019-07-18 18:34:51 +0530

    Update Simple_linkedlist.cpp

--- Simple_linkedlist.cpp
+++ Simple_linkedlist.cpp
@@ -1,42 +1,59 @@
-typedef struct Node
-{
-    int data;
-    Node *next;
-};
-Node *head;
-void add_list(int value)
-{
-    Node *curret,*temp = new Node;
-    temp->data = value;
-    temp ->next = NULL;
-      if(head == NULL)
-      {
-       head = temp;
-       curret = temp;   // cout<<curret << " = " << curret->data << endl;
-      }
-      else
-      {
-          curret ->next = temp;
-          curret = temp;//  cout<<curret << " = " << curret->data <<endl;
-      }
-}
-void print()
-{
-    Node *temp = head;
-    while(temp != NULL)
-    {
-        cout << temp->data <<endl;
-        temp = temp -> next;
-    }
-}
+#include <iostream>
+using namespace std;
 
-int main()
-{
-    for(int i = 1; i <= 20 ; i++)
-    {
-    add_list(i);
-    }
-    print();
-
-return 0;
-}
+ 
+ typedef struct Node
+ {
+     int data;
+     Node *next;
+ };
+ Node *head;
+ void cre(int value)
+ {
+     Node *curr,*temp = (Node*)malloc(sizeof(Node));
+     temp ->data = value;
+     temp ->next = NULL;
+     if(head == NULL)
+     {
+         head = temp;
+         curr = temp;
+     }
+     else
+     {
+         curr ->next = temp;
+         curr = temp;
+     }
+ }
+ void rav()
+ {
+     Node *prev = NULL;
+     Node*curr = head;
+     Node*next = NULL;
+     while(curr != NULL)
+     {
+         next = curr ->next;
+         curr ->next = prev;
+         prev = curr;
+         curr = next;
+     }
+     head = prev;
+ }
+ void dis()
+ {
+     Node *temp = head;
+     while(temp != NULL)
+     {
+       std::cout << temp ->data << std::endl;
+         temp = temp ->next;
+     }
+ }
+ int main()
+ {
+     for(int i = 0;i<10;i++)
+     {
+         cre(i);
+     }
+     rav();
+     dis();
+     return 0;
+ }
