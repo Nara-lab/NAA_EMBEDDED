700141696e004dfb6c353d2494267aed747eeeb7
Narasimha Reddy MV <39734170+Narasimha012@users.noreply.github.com>
2019-07-18 16:20:03 +0530

    Update and rename Single_linkedlist.cpp to Simple_linkedlist.cpp

---
+++ Simple_linkedlist.cpp
@@ -0,0 +1,42 @@
+typedef struct Node
+{
+    int data;
+    Node *next;
+};
+  Node *head;
+void add_list(int value)
+{
+    Node *curret,*temp = new Node;
+    temp->data = value;
+    temp ->next = NULL;
+      if(head == NULL)
+      {
+       head = temp;
+       curret = temp;   // cout<<curret << " = " << curret->data << endl;
+      }
+      else
+      {
+          curret ->next = temp;
+          curret = temp;//  cout<<curret << " = " << curret->data <<endl;
+      }
+}
+void print()
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->data <<endl;
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+    for(int i = 1; i <= 20 ; i++)
+    {
+    add_list(i);
+    }
+    print();
+
+return 0;
+}
